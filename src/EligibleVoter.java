733ea14029fb7488b8760dda8ea8b90120e2df77
imran6th <imran6th@hotmail.com>
2020-09-23 21:40:08 -0400

    sfjlfjs

---
+++ src/EligibleVoter.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class EligibleVoter {
+
+    public static void main(String[] args) {
+        Scanner sc=  new Scanner(System.in);
+        System.out.println("Enter your age");
+
+        int age1=sc.nextInt();
+
+        if (age1>=18){
+            System.out.println("The person is eligible to vote");
+        }else{
+            System.out.println("The person is not eligible to vote");
+        }
+    }
+}
